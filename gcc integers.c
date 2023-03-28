2cf36554e6d1b0aa26aa6662625abbb0d84e3dac
labi <122256276+MOHAMEDlabeeb1212@users.noreply.github.com>
2023-03-28 23:51:56 +0530

    day 1

---
+++ gcc integers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf ("enter any integer u want\n");
+    scanf ("%d", &num);
+    if(num>0) {
+        printf("ur number is positive\n");
+    }
+    else if (num<0) {
+        printf("ur number is negative\n");
+    }
+    else {
+        printf ("ur number is a zero integer\n");
+    }
+    printf ("\n192211344");
+    
+    return 0;
+    
+}
